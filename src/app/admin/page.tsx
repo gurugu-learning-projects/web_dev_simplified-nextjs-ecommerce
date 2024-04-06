0a9487edbd4ab7254b3344ddaa17726fb7451767
Petro <peter.k.nets@gmail.com>
2024-04-06 15:11:27 +0300

    Show basic cards on Admin Dashboard page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,46 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  CardTitle,
+} from "@/components/ui/card";
+
+function getSalesData() {
+  return [];
+}
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="Sales" subtitle="desc" body="Text" />
+      <DashboardCard title="Products" subtitle="desc" body="Text" />
+      <DashboardCard title="Customers" subtitle="desc" body="Text" />
+      <DashboardCard title="Sales" subtitle="desc" body="Text" />
+    </div>
+  );
+}
+
+function DashboardCard({
+  title,
+  subtitle,
+  body,
+}: {
+  title: string;
+  subtitle: string;
+  body: string;
+}) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{subtitle}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{body}</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
