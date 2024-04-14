62a09526ab203be7284e2fba5f7a2ecebfae4a9f
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2024-04-14 12:20:33 +0300

    Fill out New Product form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,15 +1,48 @@
 "use client";
 
+import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { formatCurrency } from "@/lib/formatters";
+import { Button } from "@/components/ui/button";
 
 export function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<Number>();
+
   return (
-    <form>
-      <div className="">
+    <form className="space-y-8">
+      <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" className="w-full" required />
+        <Input type="text" id="name" name="name" required />
       </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price In Cents</Label>
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div className="text-muted-foreground">
+          {formatCurrency(priceInCents ? priceInCents / 100 : 0)}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" required />
+      </div>
+      <Button type="submit">Save</Button>
     </form>
   );
 }
