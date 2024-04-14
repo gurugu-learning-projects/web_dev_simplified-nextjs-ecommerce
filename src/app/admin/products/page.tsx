7e009a1afea13b207e9fc2f6dcb1f3c3911b7675
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2024-04-14 10:48:45 +0300

    Set up Product Table

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,6 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+import { PageHeader } from "../_components/PageHeader";
 
 export default function AdminProductsPage() {
   return (
@@ -11,6 +13,27 @@ export default function AdminProductsPage() {
           <Link href="/admin/products/new">New Product</Link>
         </Button>
       </div>
+      <ProductsTable />
     </>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Availble for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+    </Table>
   );
 }
