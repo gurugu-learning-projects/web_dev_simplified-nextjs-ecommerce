1cb76860a3248d940de785536b020a4c405ab77f
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2024-07-13 12:15:25 +0300

    Send an email upon successful purchase

--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -49,7 +49,13 @@ export async function POST(req: NextRequest) {
       from: `Support <${process.env.SENDER_EMAIL}>`,
       to: email,
       subject: "Order Confirmation",
-      react: <h1>Hi!</h1>,
+      react: (
+        <PurchaseReceiptEmail
+          product={product}
+          order={order}
+          downloadVerificationId={downloadVerification.id}
+        />
+      ),
     });
   }
 
---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product name",
+    description: "Some description",
+    imagePath:
+      "/products/9a6ca4b6-5cc7-4add-8feb-34cbd72a7f82-photo_2024-05-15_19-48-47.jpg",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
